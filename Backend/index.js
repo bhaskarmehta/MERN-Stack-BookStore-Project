0135ca8c94c710533ab2195a0d19252c4edb926f
bhaskar <bhaskarmehta422@gmail.com>
2024-04-29 11:29:48 +0530

    Backed code

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.get('/',(req,res)=>{
+    res.send("I am from Express")
+});
+
+app.get('/about',(req,res)=>{
+    res.send("I am from Express About")
+});
+
+app.listen(PORT,()=>{
+    console.log("Server is running")
+})
